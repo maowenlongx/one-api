791d7d6c0063a126bce4646aec56d2d78bfcc9d8
maowenlongx <maowenlongx@gmail.com>
2024-10-21 22:59:30 +0800

    Update cache.go

--- model/cache.go
+++ model/cache.go
@@ -32,7 +32,7 @@ func CacheGetTokenByKey(key string) (*Token, error) {
 	}
 	var token Token
 	if !common.RedisEnabled {
-//		err := DB.Where(keyCol+" = ?", key).First(&token).Error
+		err := DB.Where(keyCol+" = ?", key).First(&token).Error
 
 		return &token, err
 	}
